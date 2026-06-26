bb8e1744516525c7071f4616194b480716413f1b
Michael Tolulope <olagunjum99@gmail.com>
2026-06-26 17:50:19 +0100

    fixing issue

--- src/app/producer/page.tsx
+++ src/app/producer/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { createClient } from '@supabase/ssr';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
